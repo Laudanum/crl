4baa8eaecbec6de4ce12b986a37d235bc6868f5e
Mr Snow <mr.snow@houseoflaudanum.com>
2012-05-30 10:59:29 +1000

    Update widget parameters to match theme.

--- themes/robota/functions.php
+++ themes/robota/functions.php
@@ -1,7 +1,13 @@
 <?php
 
-register_sidebar( array(
+register_sidebar(
+	array(
     'id'          => 'front-page',
     'name'        => __( 'Front page', $text_domain ),
     'description' => __( 'This sidebar is only used on the front page for the centre content area.', $text_domain ),
-) );
+	'before_widget' => '<article id="%1$s" class="widget %2$s">',
+	'after_widget'  => '</article>',
+	'before_title'  => '<header class="entry-header"><h2 class="entry-title widgettitle">',
+	'after_title'   => '</h2></header>',
+	)
+);
