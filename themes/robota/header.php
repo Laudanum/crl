92927b386941c0c593c790d7207d34631c103b51
Eri Swager <eri@houseoflaudanum.com>
2012-08-17 18:23:20 -0500

    Initial commit

--- themes/robota/header.php
+++ themes/robota/header.php
@@ -10,26 +10,21 @@
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class() ?>>
 	<div id="wrapper">
-		<header id="header">
-			<<?php esplanade_title_tag( 'site' ); ?> id="site-title"><a href="<?php echo home_url( '/' ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></<?php esplanade_title_tag( 'site' ); ?>>
-			<?php if( ! is_active_sidebar( 1 ) ) : ?>
-				<<?php esplanade_title_tag( 'desc' ); ?> id="site-description"><?php bloginfo( 'description' ); ?></<?php esplanade_title_tag( 'desc' ); ?>>
-			<?php endif; ?>
-			<?php get_sidebar( 'header' ); ?>
-			<div class="clear"></div>
-			<?php if ( ( '' != get_header_image() ) ||  ( false != get_header_image() ) ) : ?>
-				<a href="<?php echo home_url( '/' ); ?>" rel="home">
-					<img id="header-image" src="<?php header_image(); ?>" alt="<?php bloginfo( 'name' ); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" />
-				</a>
-			<?php endif; ?>
-			<nav id="access">
-				<a class="nav-toggle" href="#">Navigation</a>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary_nav' ) ); ?>
-				<?php get_search_form(); ?>
-				<div class="clear"></div>
-			</nav><!-- #access -->
-		</header><!-- #header -->
+    	<div id="header-wrap">
+            <header id="header">
+                <<?php esplanade_title_tag( 'site' ); ?> id="site-title"><a href="<?php echo home_url( '/' ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></<?php esplanade_title_tag( 'site' ); ?>>
+                <?php if( ! is_active_sidebar( 1 ) ) : ?>
+                    <<?php esplanade_title_tag( 'desc' ); ?> id="site-description"><?php bloginfo( 'description' ); ?></<?php esplanade_title_tag( 'desc' ); ?>>
+                <?php endif; ?>
+                
+                <nav id="access">
+                    <a class="nav-toggle" href="#">Navigation</a>
+                    <?php wp_nav_menu( array( 'theme_location' => 'primary_nav' ) ); ?>
+                    <div class="clear"></div>
+                </nav><!-- #access -->
+        </div><!-- #header-wrap -->
--- themes/robota/slider.php
+++ themes/robota/slider.php
@@ -11,25 +11,13 @@
 								<?php esplanade_post_video(); ?>
 							<?php elseif( has_post_thumbnail() ) : ?>
 								<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-									<?php the_post_thumbnail( 'slider-thumb' ); ?>
+									<?php the_post_thumbnail( 'slider-full' ); ?>
 								</a>
 							<?php endif; ?>
 							<div class="entry-container">
 								<header class="entry-header">
 									<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-									<aside class="entry-meta">
-										<?php the_time( get_option( 'date_format' ) ); ?> | 
-										<?php _e( 'Filed under', 'esplanade' ); ?>: <?php the_category( ', ' ) ?>
-										<?php edit_post_link( __( 'Edit', 'esplanade' ), ' | ', '' ); ?>
-									</aside><!-- .entry-meta -->
 								</header><!-- .entry-header -->
-								<div class="entry-summary">
-									<?php if( has_post_format( 'audio' ) ) : ?>
-										<?php esplanade_post_audio(); ?>
-									<?php endif; ?>
-									<?php the_excerpt(); ?>
-								</div><!-- .entry-summary -->
-								<div class="clear"></div>
 							</div><!-- .entry-container -->
 						</article><!-- .post -->
 					</li>
