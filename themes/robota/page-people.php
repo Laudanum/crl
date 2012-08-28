c738f8f0182cb8e415f68bf589d036abf3b6344f
Eri Swager <eri@houseoflaudanum.com>
2012-08-28 16:46:28 -0500

    #33 Fix parent post ID for people and projects template

--- themes/robota/page-people.php
+++ themes/robota/page-people.php
@@ -23,7 +23,7 @@ get_header();
 			
 			$args = 	array(
 				'post_type' 	=> 'page',
-				'post_parent'	=> '480',
+				'post_parent'	=> '418',
 				'posts_per_page'=> -1
 			);
 			$projects_query = new WP_Query ( $args );
