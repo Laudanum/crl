036fb78611f34bde76f8e5c00345c08015dac2f4
Eri Swager <eri@houseoflaudanum.com>
2012-08-28 13:14:34 -0500

    Add missing files for parent theme

--- themes/esplanade/functions.php
+++ themes/esplanade/functions.php
@@ -38,7 +38,8 @@ function esplanade_theme_setup() {
 	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
 	
 	// Add support for post thumbnails and custom image sizes specific to theme locations
-	add_theme_support( 'post-thumbnails', array( 'post' ) );
+	add_theme_support( 'post-thumbnails' ); 
+//	add_theme_support( 'post-thumbnails', array( 'post' ) );
 	add_image_size( 'slider-thumb', 640, 395, 1 );
 	add_image_size( 'blog-thumb', 268, 200, 1 );
 	add_image_size( 'teaser-thumb', 310, 190, 1 );
@@ -46,36 +47,19 @@ function esplanade_theme_setup() {
 	add_image_size( 'video-thumb', 640, 395, 1 );
 	add_image_size( 'attachment-thumb', 700, 9999 ); // no crop flag, unlimited height
 	
-	if( esplanade_is_wp_version( '3.4' ) ) {
-		// Allows users to set a custom background
-		add_theme_support( 'custom-background' );
-		
-		// Allows users to set a custom header image
-		add_theme_support( 'custom-header', array(
-			'width' => 1082,
-			'height' => esplanade_get_option( 'header_image_height' ),
-			'default-text-color' => '333',
-			'flex-height' => true,
-			'wp-head-callback' => 'esplanade_header_style',
-			'admin-head-callback' => 'esplanade_admin_header_style',
-			'admin-preview-callback' => 'esplanade_admin_header_image'
-		) );
-	} else {
-		// Allows users to set a custom background
-		add_custom_background();
-		
-		// Allows users to set a custom header image
-		if ( ! defined( 'HEADER_TEXTCOLOR' ) )
-			define( 'HEADER_TEXTCOLOR', '333' );
-		// The height and width of your custom header.
-		if ( ! defined( 'HEADER_IMAGE_WIDTH' ) )
-			define( 'HEADER_IMAGE_WIDTH', 1082 );
-		if ( ! defined( 'HEADER_IMAGE_HEIGHT' ) )
-			define( 'HEADER_IMAGE_HEIGHT', esplanade_get_option( 'header_image_height' ) );
-		// Add a way for the custom header to be styled in the admin panel
-		add_custom_image_header( 'esplanade_header_style', 'esplanade_admin_header_style', 'esplanade_admin_header_image' );
-	}
+	// Allows users to set a custom background
+	add_custom_background();
 	
+	// Allows users to set a custom header image
+	if ( ! defined( 'HEADER_TEXTCOLOR' ) )
+		define( 'HEADER_TEXTCOLOR', '333' );
+	// The height and width of your custom header.
+	if ( ! defined( 'HEADER_IMAGE_WIDTH' ) )
+		define( 'HEADER_IMAGE_WIDTH', 1082 );
+	if ( ! defined( 'HEADER_IMAGE_HEIGHT' ) )
+		define( 'HEADER_IMAGE_HEIGHT', esplanade_get_option( 'header_image_height' ) );
+	// Add a way for the custom header to be styled in the admin panel
+	add_custom_image_header( 'esplanade_header_style', 'esplanade_admin_header_style', 'esplanade_admin_header_image' );
 	
 	// Styles the post editor
 	add_editor_style();
@@ -179,7 +163,7 @@ if ( ! function_exists( 'esplanade_header_style' ) ) :
  * @since Esplanade 1.0
  */
 function esplanade_header_style() {
-	if ( '333' == get_header_textcolor() )
+	if ( HEADER_TEXTCOLOR == get_header_textcolor() )
 		return; ?>
 <style type="text/css">
 <?php if ( 'blank' == get_header_textcolor() ) : ?>
@@ -214,7 +198,7 @@ function esplanade_admin_header_style() {
 <style type="text/css">
 	@import url("http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold|Droid+Serif:regular,italic,bold,bolditalic&subset=latin");
 	.appearance_page_custom-header #headimg {
-		width:<?php echo get_custom_header()->width; ?>px;
+		width:<?php echo HEADER_IMAGE_WIDTH; ?>px;
 		border:none;
 	}
 	#headimg {
@@ -250,7 +234,7 @@ function esplanade_admin_header_style() {
 		box-shadow:0 0 3px #999;
 		background:#fff;
 	}
-<?php if ( '333' != get_header_textcolor() ) : ?>
+<?php if ( HEADER_TEXTCOLOR != get_header_textcolor() ) : ?>
 	#headimg h1 a,
 	#desc {
 		color:#<?php header_textcolor() ?>;
@@ -268,10 +252,10 @@ if ( ! function_exists( 'esplanade_admin_header_image' ) ) :
  * @since Esplanade 1.0
  */
 function esplanade_admin_header_image() {
-	if ( 'blank' == get_theme_mod( 'header_textcolor', '333' ) || '' == get_theme_mod( 'header_textcolor', '333' ) )
+	if ( 'blank' == get_theme_mod( 'header_textcolor', HEADER_TEXTCOLOR ) || '' == get_theme_mod( 'header_textcolor', HEADER_TEXTCOLOR ) )
 		$style = ' style="display:none;"';
 	else
-		$style = ' style="color:#' . get_theme_mod( 'header_textcolor', '333' ) . ';"';
+		$style = ' style="color:#' . get_theme_mod( 'header_textcolor', HEADER_TEXTCOLOR ) . ';"';
 		$header_image = get_header_image(); ?>
 <div id="headimg">
 	<h1><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
@@ -1494,6 +1478,8 @@ endif;
 
 add_filter( 'wp_get_attachment_link', 'esplanade_rel_attachment' );
 
+
+
 if ( ! function_exists( 'esplanade_get_first_image' ) ) :
 /**
  * Show the first image inserted in the current postimage
@@ -1630,7 +1616,9 @@ function esplanade_post_gallery() {
 			<?php endforeach; ?>
 			<div class="clear"></div>
 		</div><!-- .gallery -->
-		<p><?php echo sprintf( __( 'This is a gallery about %1$s and contains %2$d images.', 'esplanade' ), $link, $count ); ?></p>
+		<p>
+		<?php echo sprintf( __( 'This is a gallery about %1$s and contains %2$d images.', 'esplanade' ), $link, $count ); ?>
+		</p>
 	<?php endif;
 }
 endif;
@@ -1725,7 +1713,7 @@ function esplanade_file_types( $types ) {
 }
 endif;
 
-add_filter( 'ext2type', 'esplanade_file_types' );
+add_filter( 'ext2type', 'esplanade_mime_types' );
 
 if ( ! function_exists( 'esplanade_mime_types' ) ) :
 /**
@@ -2120,13 +2108,17 @@ endif;
 add_action( 'personal_options_update', 'esplanade_save_extra_profile_fields' );
 add_action( 'edit_user_profile_update', 'esplanade_save_extra_profile_fields' );
 
-// checks is WP is at least a certain version (makes sure it has sufficient comparison decimals
-function esplanade_is_wp_version( $is_ver ) {
-	$wp_ver = explode( '.', get_bloginfo( 'version' ) );
-	$is_ver = explode( '.', $is_ver );
-	for( $i=0; $i<=count( $is_ver ); $i++ )
-		if( !isset( $wp_ver[$i] ) ) array_push( $wp_ver, 0 );
-	foreach( $is_ver as $i => $is_val )
-		if( $wp_ver[$i] < $is_val ) return false;
-	return true;
+function people_init() {
+	// create a new taxonomy
+	register_taxonomy(
+		'people',
+		'page',
+		array(
+			'label' => __( 'People' ),
+			'sort' => true,
+			'args' => array( 'orderby' => 'term_order' ),
+			'rewrite' => array( 'slug' => 'person' )
+		)
+	);
 }
+add_action( 'init', 'people_init' );
